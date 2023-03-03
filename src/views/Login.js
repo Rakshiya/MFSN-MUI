b0290a9154e50d2e5c917e8c7e2439364e2b64d2
Rakshiya <rakshiya.siddiqui@samvadsocial.com>
2023-03-03 15:43:54 +0530

    Login Page

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,10 +1,54 @@
 import React from 'react';
+import { Box, Grid,Typography,TextField, Checkbox, Button, Stack } from '@mui/material';
+import LoginHeader from '../Components/LoginHeader';
 
 function Login(props) {
     return (
-        <div>
-            <h1>Login Page</h1>
-        </div>
+        <Box sx={{ flexGrow: 1,backgroundColor:'#f9f9f9'  }}> 
+            <LoginHeader />
+            <Grid container spacing={2} pt={2}>
+                <Grid item xs={7} sx={{textAlign: 'center'}}>
+                    <Box sx={{m:10}}>
+                        <img src={require('../assets/images/login.png')} />
+                    </Box>
+                </Grid>
+                <Grid item xs={5} sx={{textAlign: 'center',justifyContent:'center',width:"100%" }}>
+                    <Box p={2} sx={{m:10,backgroundColor:'#ffffff'}}>
+                        <Typography variant="h5" gutterBottom>
+                            Welcome To The Affiliate Portal
+                        </Typography>
+                        <Typography variant="h4" gutterBottom>
+                            User Login
+                        </Typography>
+                        <Box
+                            component="form"
+                            sx={{
+                                '& .MuiTextField-root': { m: 1, width: '50ch' },
+                            }}
+                            noValidate
+                            autoComplete="off"
+                        >
+                            <div>                            
+                                <TextField fullWidth id="outlined-basic" label="Email Address" variant="outlined" />
+                                <TextField fullWidth id="outlined-basic" label="Password" variant="outlined" />
+                            </div>
+                            <div>
+                                {/* <Stack > */}
+                                    <Checkbox />Remember Me
+                                    <Button href="#text-buttons">Forget Password?</Button>
+                                {/* </Stack> */}
+                            </div>
+                            <div>
+                                <Button fullWidth variant="contained">Login Now {'<<'}</Button>
+                            </div>
+                            
+                        </Box>
+                        
+                    </Box>
+                    
+                </Grid>
+            </Grid>
+        </Box>
     );
 }
 
