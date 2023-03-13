abc66dd47073a967a8fc1c012f68d4c34824907a
Aishwarya <aishwarya.kabade@samvadsocial.com>
2023-03-13 20:36:19 +0530

    hot leads data integration

--- frontend/src/AppConfig.jsx
+++ frontend/src/AppConfig.jsx
@@ -1,12 +1,15 @@
 
 const config ={
-    // BASE_URL:'http://54.67.80.194:3000/',
+    //  BASE_URL:'http://54.67.80.194:3000/',
+    //  MEMBER_URL:'https://member.myfreescorenow.com/',
+    //  API_BASE_URL: 'http://54.67.80.194/mfsnbackend/api',
+    BASE_URL:'http://127.0.0.1:8000',
+     MEMBER_URL:'https://member.myfreescorenow.com/',
+    API_BASE_URL: 'http://127.0.0.1:8000/api'
+    // BASE_URL:'http://192.168.1.104:3000/',
     // MEMBER_URL:'https://member.myfreescorenow.com/',
-    // API_BASE_URL: 'http://54.67.80.194/mfsnbackend/api',
-    BASE_URL:'http://192.168.1.104:3000/',
-    MEMBER_URL:'https://member.myfreescorenow.com/',
-    API_BASE_URL: 'http://localhost:8000/api',
-    RECAPTCHA_SITEKEY:'6LcxArEhAAAAAKkg1np-SpInlWTFbm0nXzSIBJ0s'
+    // API_BASE_URL: 'http://localhost:8000/api',
+    // RECAPTCHA_SITEKEY:'6LcxArEhAAAAAKkg1np-SpInlWTFbm0nXzSIBJ0s'
 }
 export default config;
 
--- frontend/src/views/Web/ManageLeadsPanel/HotLeads.js
+++ frontend/src/views/Web/ManageLeadsPanel/HotLeads.js
@@ -1,9 +1,15 @@
 import DefaultLayout from '../../../Components/DefaultLayout';
-import React,{useMemo} from 'react';
+import {useParams, useNavigate, Link} from 'react-router-dom';
+
+import React,{useMemo,useState,useEffect} from 'react';
 import {TabContext,TabPanel,TabList} from '@mui/lab';
+import swal from 'sweetalert';
+import AuthUser from "../../../Components/Auth/AuthUser";
 import HotLeadsColumns from '../../../Components/ManageLeadsColumns/HotLeadsColumns';
 import ApprovedLeadsColumns from '../../../Components/ManageLeadsColumns/ApprovedLeadsColumns';
 import { data } from '../CompanyPanel/makeData';
+//import LoaderComponent from "../../../Components/Spinner";
+
 import MaterialReactTable from 'material-react-table';
 import { AccountCircle, Send } from '@mui/icons-material';
 import {
@@ -15,77 +21,137 @@ import {
     Typography,
   } from '@mui/material';
 function HotLeads(props) {
+  const { http, user } = AuthUser();
+  const [data, setData] = useState([]);
+  const [rowSelection, setRowSelection] = useState({});
+  const [statusMessage, setStatusMesage] = useState('');
+  const [leadDetails, setleadDetails] = useState([]);
+  const [userList, setList] = useState([]);
+  const [filterUsers, setFilterUsers]=useState([]);
+  const {id} = useParams();
+ 
+  const [loader, setLoader] = useState("");
+  const [search, setSearch]=useState("");
+  const [totalRows, setTotalRows] = useState(0);
+	const [perPage, setPerPage] = useState(10);
+  const [isLoading, setIsLoading] = useState(false);
+  const q = search==''?'null':search;
+
+
+  const fetchReferredList = async  page => {
+    setIsLoading(true)
+    http.get('/fetchReferredList/'+user.id+'/'+q+'/'+perPage+'/list')
+    .then((res) => {
+      setIsLoading(false)
+      // setList(res.data.data);
+      setFilterUsers(res.data.data);
+    //setFilterList(res.data.data);
+      //  setTotalRows(res.data.total);
+      
+       setData(res.data.data);
+      
+      
+    
+       setLoader("");
+      
+    });
+  };
+  const fetchLeadDetails = () =>{
+    //setLoader(<LoaderComponent/>);
+      http.get('/leadDetails'/'+user.id+' ).then((res)=>{
+            setleadDetails(res.data); 
+          //  setLoader('');
+      })
+  }
+  const sendAgreement = ((id)=>{
+    
+    document.getElementById("operationbtn").classList.add('d-none');
+    setStatusMesage('Sending Agreement...');
+   // setLoader(<LoaderComponent/>);
+    
+   http.post('/sendAgreement/?id='+id+'').then((res)=>{
+     console.log(res.data);
+//       document.getElementById("operationbtn").classList.remove('d-none');
+//       swal({
+//         title: "Success",
+//         text: "Agreement Successfully Sent.",
+//         icon: "success",
+//         button: "Ok",
+//       }).then((ok)=>{
+//         if(ok){
+//           fetchLeadDetails()
+//         }
+//       });
+//  //     setLoader('');
+//       setStatusMesage('');
+//       document.getElementById("operationbtn").classList.remove('d-none'); 
+    }).catch(error => {
+      // console.log(error)
+      setStatusMesage('');
+      setLoader('');
+      document.getElementById("operationbtn").classList.remove('d-none');
+  })
+});
+ 
+
+  useEffect(() => {
+    fetchReferredList();
+  }, []);
+  useEffect(()=>{
+    const result = fetchReferredList()
+    setFilterUsers(result); 
+  },[search])
+  useEffect(()=>{
+    fetchLeadDetails();
+},[])
+  useEffect(() => {
+    //do something when the row selection changes...
+    console.info({ rowSelection });
+  }, [rowSelection]);
+  console.log(data);
     const HotLeadsColumns = useMemo(
         () => [
           {
             id: 'employee', //id used to define `group` column
-            header: 'Employee',
+            header: '',
             columns: [
               {
-                accessorFn: (row) => `${row.firstName} ${row.lastName}`, //accessorFn used to join multiple data into a single cell
-                id: 'name', //id is still required when using accessorFn instead of accessorKey
-                header: 'Name',
+                accessorFn: (data) => `${data.first_name+' '+data.last_name} `, //accessorFn used to join multiple data into a single cell
+                id: 'First Name', //id is still required when using accessorFn instead of accessorKey
+                header: 'First Name',
+                size: 200,
+              },
+              
+              {
+                accessorFn: (data) => `${data.refernce_affiliates_id} `, //accessorFn used to join multiple data into a single cell
+               id: 'Email', //id is still required when using accessorFn instead of accessorKey
+               header: 'Email',
+               size: 200,
+            },
+               {
+                accessorFn: (data) => `${data.phone_no}`, //accessorFn used to join multiple data into a single cell
+                id: 'Phone Number', //id is still required when using accessorFn instead of accessorKey
+                header: 'Phone Number',
+                size: 200,
+               },
+             
+              {
+                accessorFn: (data) => `${data.status_name}`, //accessorFn used to join multiple data into a single cell
+                id: 'Status', //id is still required when using accessorFn instead of accessorKey
+                header: 'Status',
                 size: 200,
               },
               {
-                accessorKey: 'email', //accessorKey used to define `data` column. `id` gets set to accessorKey automatically
-                enableClickToCopy: true,
-                header: 'Email',
+                accessorFn: (data) => (`${data.status_name}` == "Submitted") ? <button onClick={()=>{sendAgreement(data.company_id)}}  id="operationbtn" >send Agreement </button> : "", //accessorFn used to join multiple data into a single cell
+                id: 'Action', //id is still required when using accessorFn instead of accessorKey
+                header: 'Action',
                 size: 200,
               },
             ],
           },
-          {
-            id: 'id',
-            header: 'Job Info',
-            columns: [
-              {
-                accessorKey: 'salary',
-                filterVariant: 'range',
-                header: 'Salary',
-                size: 200,
-                //custom conditional format and styling
-                Cell: ({ cell }) => (
-                  <Box
-                    component="span"
-                    sx={(theme) => ({
-                      backgroundColor:
-                        cell.getValue() < 50_000
-                          ? theme.palette.error.dark
-                          : cell.getValue() >= 50_000 && cell.getValue() < 75_000
-                          ? theme.palette.warning.dark
-                          : theme.palette.success.dark,
-                      borderRadius: '0.25rem',
-                      color: '#fff',
-                      maxWidth: '9ch',
-                      p: '0.25rem',
-                    })}
-                  >
-                    {cell.getValue()?.toLocaleString?.('en-US', {
-                      style: 'currency',
-                      currency: 'USD',
-                      minimumFractionDigits: 0,
-                      maximumFractionDigits: 0,
-                    })}
-                  </Box>
-                ),
-              },
-              {
-                accessorKey: 'jobTitle', //hey a simple column for once
-                header: 'Job Title',
-                size: 200,
-              },
-              {
-                accessorFn: (row) => new Date(row.startDate), //convert to Date for sorting and filtering
-                id: 'startDate',
-                header: 'Start Date',
-                filterFn: 'lessThanOrEqualTo',
-                sortingFn: 'datetime',
-                Cell: ({ cell }) => cell.getValue()?.toLocaleDateString(), //render Date as a string
-                Header: ({ column }) => <em>{column.columnDef.header}</em>, //custom header markup
-              },
-            ],
-          },
+          
+        
+       
         ],
         [],
       );
@@ -109,57 +175,7 @@ function HotLeads(props) {
               },
             ],
           },
-          {
-            id: 'id',
-            header: 'Job Info',
-            columns: [
-              {
-                accessorKey: 'salary',
-                filterVariant: 'range',
-                header: 'Salary',
-                size: 200,
-                //custom conditional format and styling
-                Cell: ({ cell }) => (
-                  <Box
-                    component="span"
-                    sx={(theme) => ({
-                      backgroundColor:
-                        cell.getValue() < 50_000
-                          ? theme.palette.error.dark
-                          : cell.getValue() >= 50_000 && cell.getValue() < 75_000
-                          ? theme.palette.warning.dark
-                          : theme.palette.success.dark,
-                      borderRadius: '0.25rem',
-                      color: '#fff',
-                      maxWidth: '9ch',
-                      p: '0.25rem',
-                    })}
-                  >
-                    {cell.getValue()?.toLocaleString?.('en-US', {
-                      style: 'currency',
-                      currency: 'USD',
-                      minimumFractionDigits: 0,
-                      maximumFractionDigits: 0,
-                    })}
-                  </Box>
-                ),
-              },
-              {
-                accessorKey: 'jobTitle', //hey a simple column for once
-                header: 'Job Title',
-                size: 200,
-              },
-              {
-                accessorFn: (row) => new Date(row.startDate), //convert to Date for sorting and filtering
-                id: 'startDate',
-                header: 'Start Date',
-                filterFn: 'lessThanOrEqualTo',
-                sortingFn: 'datetime',
-                Cell: ({ cell }) => cell.getValue()?.toLocaleDateString(), //render Date as a string
-                Header: ({ column }) => <em>{column.columnDef.header}</em>, //custom header markup
-              },
-            ],
-          },
+         
         ],
         [],
       );
@@ -229,9 +245,10 @@ function HotLeads(props) {
                         enablePinning
                         enableRowActions
                         enableRowSelection
+                        state={{ rowSelection,isLoading }} 
                         initialState={{ showColumnFilters: false }}
                         positionToolbarAlertBanner="bottom"
-                        renderDetailPanel={({ row }) => (
+                        renderDetailPanel={({res}) => (
                         <Box
                             sx={{
                             display: 'flex',
@@ -243,7 +260,7 @@ function HotLeads(props) {
                             <Box sx={{ textAlign: 'center' }}>
                             <Typography variant="h4">Signature Catch Phrase:</Typography>
                             <Typography variant="h6">
-                                &quot;{row.original.signatureCatchPhrase}&quot;
+                                &quot;{res.original.signatureCatchPhrase}&quot;
                             </Typography>
                             </Box>
                         </Box>
@@ -277,7 +294,7 @@ function HotLeads(props) {
                         </MenuItem>,
                         ]}
                         
-                    />
+                    /> 
                   </TabPanel>
                   <TabPanel value="4">Completed</TabPanel>
                   <TabPanel value="5">Deferred</TabPanel>
