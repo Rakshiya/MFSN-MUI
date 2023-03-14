2e30f6d453b929955fe409a98b33a2e26a89ef8f
arun-samvadsocial <arun.vishwakarma@samvadsocial.com>
2023-03-14 12:09:48 +0530

    add 1 space

--- frontend/src/AppConfig.jsx
+++ frontend/src/AppConfig.jsx
@@ -9,7 +9,7 @@ const config ={
     // BASE_URL:'http://192.168.1.104:3000/',
     // MEMBER_URL:'https://member.myfreescorenow.com/',
     // API_BASE_URL: 'http://localhost:8000/api',
-    // RECAPTCHA_SITEKEY:'6LcxArEhAAAAAKkg1np-SpInlWTFbm0nXzSIBJ0s'
+    // RECAPTCHA_SITEKEY:'6LcxArEhAAAAAKkg1np-SpInlWTFbm0nXzSIBJ0s' 
 }
 export default config;
 
